9b61dc09ccce51fce894c26b7cbfab14e9bebaaf
Hem Shrestha <hereshem@gmail.com>
2020-06-20 14:38:03 +0545

    app.py for python project

---
+++ README.md
@@ -0,0 +1,17 @@
+## Setup Instruction
+
+### Using `requirements.txt`
+- `pip install -r requirements.txt`
+- `python app.py`
+
+
+### Using `Pipfile`
+- `pip install pipenv`
+- `pipenv install`
+- `pipenv run python app.py`
+
+
+### Using Poetry `pyproject.toml`
+- `pip install poetry`
+- `poetry install`
+- `poetry run python app.py`
---
+++ app.py
@@ -0,0 +1,21 @@
+import time
+from flask import Flask
+app = Flask(__name__)
+
+START = time.time()
+
+def elapsed():
+    running = time.time() - START
+    minutes, seconds = divmod(running, 60)
+    hours, minutes = divmod(minutes, 60)
+    return "%d:%02d:%02d" % (hours, minutes, seconds)
+
+@app.route('/')
+def root():
+    return "Hello World (Python)! (up %s)\n" % elapsed()
+
+def main():
+   app.run(debug=True, host="0.0.0.0", port=3000)
+
+if __name__ == "__main__":
+    main()
